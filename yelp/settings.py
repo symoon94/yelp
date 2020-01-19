6325a0707a50472b145a3379a7f0afb6ad69071a
symoon94 <msy0128@gmail.com>
2020-01-19 07:34:55 -0700

    md set

--- yelp/settings.py
+++ yelp/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'cztheday25',
-        'USER': 'symoon94',
+        'USER': 'sooyoungmoon',
         'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '',
